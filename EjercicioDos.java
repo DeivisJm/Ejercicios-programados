db91ce52f8e0101b2fdfbb17e3eb2f0e33fc51af
DeivisJm <deivisjm1205@gmail.com>
2023-05-17 11:57:45 -0600

    Ejercicio 1 y 2

---
+++ EjercicioDos.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EjercicioUno{
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un valor de celsius");
+        int celsius = input.nextInt();
+        double fahrenheit = (9.0/5)* celsius + 32
+        System.out.println("El resutado es: "+ fahrenheit)
+    }
+}
+    
+       
